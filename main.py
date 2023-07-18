f597a9deacd6b803c5dd3fddc1e66b5dff6eb67b
reza <mardanireza58@gmail.com>
2023-07-18 08:52:36 +0330

    initial class

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 class DataBase:
-    pass
+    def __init__(self, name, number) -> None:
+        self.name = name
+        self.number = number
+
+    def __str__(self) -> str:
+        return f"{self.name} : {self.number}"
+    
+p1 = DataBase("ali", 45788)
+print(p1)
