fa08a44bd831b980a2c25ba69bf39389d0a8d047
reza <mardanireza58@gmail.com>
2023-07-20 09:10:38 +0330

    update circle file

--- class-circle.py
+++ class-circle.py
@@ -4,9 +4,10 @@ class Circle:
     def __init__(self, radius) -> None:
         self.redius = radius
 
+    # محاسبه مساحت
     def environment(self):
         print((self.redius**2) * self.number_of_p)
-
+    # محاسبه محیط
     def area(self):
         print((2 * self.redius) * self.number_of_p)
 
