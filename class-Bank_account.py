031a6a55d700f147f7acd2f317b63f2498045077
reza <mardanireza58@gmail.com>
2023-07-21 10:32:47 +0330

    iadd method added

--- class-Bank_account.py
+++ class-Bank_account.py
@@ -1,3 +1,5 @@
+import os
+os.system("cls")
 class BankAccount:
     def __init__(self, name:str, balance:int=0) -> None:
         self.name = name
@@ -14,4 +16,31 @@ class BankAccount:
         self.balance += amount
 
     def __add__(self, other):
-        return f"owner: {self.name}+{other.name} --> {self.balance + other.balance} "
+        return f"owner: {self.name}+{other.name} | amount: {self.balance + other.balance} "
+    
+    def __iadd__(self, other):
+        self.balance += other.balance
+        other.balance = 0
+        return self
+    
+    def __eq__(self, other):
+        return self.balance == other.balance
+    
+    def __str__(self) -> str:
+        return f"owner: {self.name} | amount: {self.balance}"
+    
+john_acc = BankAccount('john', 1000)
+david_acc = BankAccount('david', 1000)
+
+print(john_acc+david_acc)
+
+john_acc.transfer(100, david_acc)
+
+david_acc.deposit(4000)
+
+john_acc += david_acc
+
+print(john_acc == david_acc)
+
+print(john_acc)
+print(david_acc)
