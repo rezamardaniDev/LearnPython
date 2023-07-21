4dd3a888c33345b801fe611386d8b4dfb04cdae5
reza <mardanireza58@gmail.com>
2023-07-21 10:15:29 +0330

    add some method

--- class-Bank_account.py
+++ class-Bank_account.py
@@ -2,3 +2,16 @@ class BankAccount:
     def __init__(self, name:str, balance:int=0) -> None:
         self.name = name
         self.balance = balance
+    
+    def transfer(self,amount, target):
+        self.balance -= amount
+        target.balance += amount
+    
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def __add__(self, other):
+        return f"owner: {self.name}+{other.name} --> {self.balance + other.balance} "
