e3a240918b4d4ae62e39342859671ee6314e6fdc
reza <mardanireza58@gmail.com>
2023-07-22 17:49:09 +0330

    update str-trick

---
+++ string/str-trick.py
@@ -0,0 +1,15 @@
+my_list = [2, 3, 4]
+print(max(my_list))
+print(min(my_list))
+# ----------------------------------- #
+user_age = input("enter your age: ")
+user_age=int(user_age)
+print(user_age, type(user_age))
+# ----------------------------------- #
+my_list = [201, 13, 455, 4]
+my_list.sort()
+print(my_list)
+# ----------------------------------- #
+x = 458
+x += 12
+print(x)
