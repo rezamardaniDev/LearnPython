2bb866699cb5a069ddb76e17932a9c7fb3f982c6
reza <mardanireza58@gmail.com>
2023-08-19 20:25:14 +0330

    update

--- erro handeling/erro_t1.py
+++ erro handeling/erro_t1.py
@@ -2,8 +2,10 @@ while True:
     try:
         user_input = int(input("please enter your age: "))
         break
+    # if user_input is valid, return user_input
     except:
         print("your input is invalid try again")
+        # if user_input is invalid , ask again with error message
 
 
 print(f"your name is {user_input}")
