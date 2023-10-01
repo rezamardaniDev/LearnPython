8f1a73d5de8bd439a23506addc066e16f1ea18a2
reza <mardanireza58@gmail.com>
2023-10-01 22:33:37 +0330

    add args and kwargs

--- decorators/main.py
+++ decorators/main.py
@@ -1,13 +1,13 @@
 def uppercase(func):
-    def wrapper():
-        original_result = func()
+    def wrapper(*args, **kwargs):
+        original_result = func(*args, **kwargs)
         modified_result = original_result.upper()
         return modified_result
     return wrapper
 
 @uppercase
-def greet():
-    return "hello"
+def greet(name):
+    return f"hello {name}"
 
-print(greet())
+print(greet("REZA"))
 
